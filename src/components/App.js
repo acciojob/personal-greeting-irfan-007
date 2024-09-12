36be740e22b9da95690a45251ad91619b457607a
irfan-007 <thanatos786687@gmail.com>
2024-09-12 23:05:32 +0530

    all

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [text, setText] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+      Enter your name:
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      {text}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
